37a8e17b8e322902b342d3c79a800c074be41b58
liamylian <liamylian@163.com>
2020-07-31 15:16:29 +0800

    opt route

--- handler/http/api.go
+++ handler/http/api.go
@@ -21,41 +21,41 @@ func NewHandler(ucase usecase.UseCase) *handler {
 }
 
 func (h *handler) Route(e *echo.Echo) {
-	api := e.Group("/api")
-
+	bucketMux := NewQueryMux()
+	e.Any("/", bucketMux.Handle())
 	// 桶列表
-	api.GET("/vendors/:vendor/buckets", h.listBucket)
+	bucketMux.GET("?vendor=:vendor_name", h.listBucket)
 	// 创建桶
-	api.POST("/vendors/:vendor/buckets", h.createBucket)
+	bucketMux.POST("?vendor=:vendor_name", h.createBucket)
 	// 桶详情
-	api.GET("/vendors/:vendor/buckets/:bucket_name", h.getBucket)
+	bucketMux.GET("?vendor=:vendor_name&bucket=:bucket_name", h.getBucket)
 	// 删除桶
-	api.DELETE("/vendors/:vendor/buckets/:bucket_name", h.deleteBucket)
+	bucketMux.DELETE("?vendor=:vendor_name&bucket=:bucket_name", h.deleteBucket)
 
-	// 创建对象
-	api.POST("/vendors/:vendor/buckets/:bucket_name/objects", h.createObject)
+	objectMux := NewQueryMux()
+	e.Any("/:object_key", objectMux.Handle())
 	// 对象详情
-	api.HEAD("/vendors/:vendor/buckets/:bucket_name/objects/:object_key", h.headObject)
+	objectMux.HEAD("?vendor=:vendor_name&bucket=:bucket_name", h.headObject)
 	// 删除对象
-	api.DELETE("/vendors/:vendor/buckets/:bucket_name/objects/:object_key", h.deleteObject)
+	objectMux.DELETE("?vendor=:vendor_name&bucket=:bucket_name", h.deleteObject)
 	// 表单上传
-	api.POST("/vendors/:vendor/buckets/:bucket_name/objects/:object_key", h.postObject)
+	objectMux.POST("?vendor=:vendor_name&bucket=:bucket_name", h.postObject)
 	// 流式上传
-	api.PUT("/vendors/:vendor/buckets/:bucket_name/objects/:object_key", h.putObject)
+	objectMux.PUT("?vendor=:vendor_name&bucket=:bucket_name", h.putObject)
 	// 初始化分段上传
-	api.POST("/vendors/:vendor/buckets/:bucket_name/objects/:object_key/uploads", h.initMultipartUpload)
+	objectMux.POST("?vendor=:vendor_name&bucket=:bucket_name&uploads", h.initMultipartUpload)
 	// 完成分段上传
-	api.POST("/vendors/:vendor/buckets/:bucket_name/objects/:object_key/uploads/:upload_id/eof", h.completeMultipartUpload)
+	objectMux.POST("?vendor=:vendor_name&bucket=:bucket_name&upload_id=:upload_id&eof", h.completeMultipartUpload)
 	// 取消分段上传
-	api.DELETE("/vendors/:vendor/buckets/:bucket_name/objects/:object_key/uploads/:upload_id", h.abortMultipartUpload)
+	objectMux.DELETE("?vendor=:vendor_name&bucket=:bucket_name&upload_id=:upload_id", h.abortMultipartUpload)
 	// 上传分段
-	api.POST("/vendors/:vendor/buckets/:bucket_name/objects/:object_key/uploads/:upload_id/parts/:part_id", h.uploadPart)
+	objectMux.POST("?vendor=:vendor_name&bucket=:bucket_name&upload_id=:upload_id&part_id=:part_id", h.uploadPart)
 	// 查看分段列表
-	api.GET("/vendors/:vendor/buckets/:bucket_name/objects/:object_key/uploads/:upload_id/parts", h.listParts)
+	objectMux.GET("?vendor=:vendor_name&bucket=:bucket_name&upload_id=:upload_id&parts", h.listParts)
 }
 
 func (h *handler) listBucket(c echo.Context) error {
-	vendor := c.Param("vendor")
+	vendor := c.QueryParam("vendor_name")
 	page, pageSize := getPaging(c)
 
 	buckets, err := h.ucase.ListBucket(vendor, page, pageSize)
@@ -69,8 +69,8 @@ func (h *handler) listBucket(c echo.Context) error {
 }
 
 func (h *handler) getBucket(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 
 	bucket, err := h.ucase.GetBucket(vendor, bucketName)
 	if err != nil {
@@ -83,8 +83,8 @@ func (h *handler) getBucket(c echo.Context) error {
 }
 
 func (h *handler) createBucket(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 
 	post := &Bucket{}
 	if err := c.Bind(post); err != nil {
@@ -105,8 +105,8 @@ func (h *handler) createBucket(c echo.Context) error {
 }
 
 func (h *handler) deleteBucket(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 
 	if err := h.ucase.DeleteBucket(vendor, bucketName); err != nil {
 		return err
@@ -116,8 +116,8 @@ func (h *handler) deleteBucket(c echo.Context) error {
 }
 
 func (h *handler) headObject(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 
 	object, err := h.ucase.HeadObject(vendor, bucketName, objectKey)
@@ -134,8 +134,8 @@ func (h *handler) headObject(c echo.Context) error {
 // 生成的临时文件会在请求结束时自动删除。
 // 需确保临时文件目录有足够的存储空间，否则上传将失败。
 func (h *handler) createObject(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 
 	f, fh, err := c.Request().FormFile("file")
@@ -166,8 +166,8 @@ func (h *handler) createObject(c echo.Context) error {
 // 生成的临时文件会在请求结束时自动删除。
 // 需确保临时文件目录有足够的存储空间，否则上传将失败。
 func (h *handler) postObject(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 
 	f, fh, err := c.Request().FormFile("file")
@@ -197,8 +197,8 @@ func (h *handler) postObject(c echo.Context) error {
 // 流式上传，不消耗内存及磁盘，即服务器从请求流读取数据，并将读取到的数据直接上传到对象存储。
 // 相比表单上传方式，流式上传的失败概率会增加。因为只要上传中，读取连接或上传连接失败，即上传失败。
 func (h *handler) putObject(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 	body := c.Request().Body
 	defer body.Close()
@@ -218,8 +218,8 @@ func (h *handler) putObject(c echo.Context) error {
 }
 
 func (h *handler) deleteObject(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 
 	if err := h.ucase.DeleteObject(vendor, bucketName, objectKey); err != nil {
@@ -230,8 +230,8 @@ func (h *handler) deleteObject(c echo.Context) error {
 }
 
 func (h *handler) initMultipartUpload(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
 
 	uploadID, err := h.ucase.InitMultipartUpload(vendor, bucketName, objectKey)
@@ -245,10 +245,10 @@ func (h *handler) initMultipartUpload(c echo.Context) error {
 }
 
 func (h *handler) completeMultipartUpload(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
-	uploadID := c.Param("upload_id")
+	uploadID := c.QueryParam("upload_id")
 
 	post := struct {
 		CompleteParts []*model.ObjectCompletePart `json:"complete_parts"`
@@ -265,10 +265,10 @@ func (h *handler) completeMultipartUpload(c echo.Context) error {
 }
 
 func (h *handler) abortMultipartUpload(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
-	uploadID := c.Param("upload_id")
+	uploadID := c.QueryParam("upload_id")
 
 	if err := h.ucase.AbortMultipartUpload(vendor, bucketName, objectKey, uploadID); err != nil {
 		return err
@@ -278,11 +278,11 @@ func (h *handler) abortMultipartUpload(c echo.Context) error {
 }
 
 func (h *handler) uploadPart(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
-	uploadID := c.Param("upload_id")
-	partNumber := getInt(c.Param("part_id"), 0)
+	uploadID := c.QueryParam("upload_id")
+	partNumber := getInt(c.QueryParam("part_id"), 0)
 
 	f, fh, err := c.Request().FormFile("file")
 	if err != nil {
@@ -309,10 +309,10 @@ func (h *handler) uploadPart(c echo.Context) error {
 }
 
 func (h *handler) listParts(c echo.Context) error {
-	vendor := c.Param("vendor")
-	bucketName := c.Param("bucket_name")
+	vendor := c.QueryParam("vendor_name")
+	bucketName := c.QueryParam("bucket_name")
 	objectKey := c.Param("object_key")
-	uploadID := c.Param("upload_id")
+	uploadID := c.QueryParam("upload_id")
 
 	parts, err := h.ucase.ListParts(vendor, bucketName, objectKey, uploadID)
 	if err != nil {
